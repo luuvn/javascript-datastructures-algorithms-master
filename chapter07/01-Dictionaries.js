6620670c7cf2948cb83fc1fb3ba04688374d3214
luu <luuvn91@gmail.com>
2018-09-20 11:54:15 +0700

    update

--- chapter07/01-Dictionaries.js
+++ chapter07/01-Dictionaries.js
@@ -1,10 +1,20 @@
+class ValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+    toString() {
+        return `[#${this.key}: ${this.value}]`;
+    }
+}
+
 function Dictionary() {
 
     var items = {};
 
     this.set = function (key, value) {
         if (key != undefined && value != undefined) {
-            items[key] = value;
+            items[key] = new ValuePair(key, value);
 
             return true;
         }
@@ -16,12 +26,15 @@ function Dictionary() {
 
     };
 
-    this.has = function (key) {
+    this.hasKey = function (key) {
         return Object.prototype.hasOwnProperty.call(items, key);
     };
 
     this.get = function (key) {
-        return items[key];
+        if (items[key])
+            return items[key].value;
+
+        return undefined;
     };
 
     this.clear = function () {
@@ -33,24 +46,51 @@ function Dictionary() {
     };
 
     this.keys = function () {
-        return Object.keys(items);
+        return Object.keys(items).map(element => items[element].key);
     };
 
     this.values = function () {
-
+        return Object.keys(items).map(element => items[element].value);
     };
 
-    this.each = function (fn) {
+    this.keyValues = function () {
+        return Object.keys(items).map(element => items[element]);
+    };
 
+    this.forEach = function (fn) {
+        // Object.keys(items).every(element => fn(items[element].key, items[element].value));
+
+        let valuePairs = this.keyValues();
+        let result;
+        for (let pos in valuePairs) {
+            result = fn(valuePairs[pos].key, valuePairs[pos].value);
+            if (result == false)
+                break;
+        }
     };
 
     this.getItems = function () {
 
-    }
+    };
 
     this.isEmpty = function () {
         return this.size() == 0;
-    }
+    };
+
+    this.remove = function (key) {
+        if (items[key]) {
+            delete items[key];
+            return true;
+        }
+
+        return false;
+    };
+
+    this.toString = function () {
+        let keyValues = Object.keys(items).map(element => items[element].toString());
+
+        return keyValues.toString();
+    };
 }
 
 module.exports = Dictionary;
--- chapter07/UnitTest.js
+++ chapter07/UnitTest.js
@@ -1,6 +1,16 @@
 let expect = require('chai').expect;
 let Dictionary = require('./01-Dictionaries.js');
 
+class MyObj {
+    constructor(el1, el2) {
+        this.el1 = el1;
+        this.el2 = el2;
+    }
+    toString() {
+        return `${this.el1.toString()}|${this.el2.toString()}`;
+    }
+}
+
 describe('Dictionary', () => {
     let dictionary;
     beforeEach(() => {
